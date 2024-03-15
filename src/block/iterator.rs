a20adb18ca40f1a80973b5fd867f63f296fa698b
zkthernum <kevin91cooland@gmail.com>
2024-03-15 12:55:11 +0800

    add  for Block

--- src/block/iterator.rs
+++ src/block/iterator.rs
@@ -9,22 +9,27 @@ use super::Block;
 pub struct BlockIterator {
     // reference to the block
     block: Arc<Block>,
-    // the current key at the iterator position
+    // the `current key` at the iterator position
     key: KeyVec,
+    // the ` first key ` in the block.
+    first_key: KeyVec,
     // the value range from the block
     value_range: (usize, usize),
     // the current index at the iterator pos.
     idx: usize,
-    // the first key in the block.
-    first_key: KeyVec,
 }
 
 impl Block {
     fn get_first_key(&self) -> KeyVec {
+        // get data stored in buffer.
         let mut buf = &self.data[..];
+        // get the common_prefix.
         buf.get_u16();
+        // get the key_len.
         let key_len = buf.get_u16();
+        // get the key.
         let key = &buf[..key_len as usize];
+        // type convert: from Key to KeyVec.
         KeyVec::from_vec(key.to_vec())
     }
 }
