de1e27a26cfe82342ed7ef9ed0d77ed08450c4ac
zkthernum <kevin91cooland@gmail.com>
2024-03-16 14:58:49 +0800

    review and add comments for MemTable mod.

--- src/mem_table.rs
+++ src/mem_table.rs
@@ -14,12 +14,19 @@ use std::sync::Arc;
 
 use crate::iterators::StorageIterator;
 use crate::key::{Key, KeySlice};
+
+/// Data Structure 1: MemTable in the Memory.
 pub struct MemTable {
+    // store the key-value pairs, inside it'a SkipMap.
     map: Arc<SkipMap<Bytes, Bytes>>,
+    // index of the current memtable.
     id: usize,
+    // the approximate_size of the current table.
     approximate_size: Arc<AtomicUsize>,
 }
+
 impl MemTable {
+    /// create a new memtable with a `specified index`.
     pub fn create(id: usize) -> Self {
         Self {
             id,
@@ -27,33 +34,46 @@ impl MemTable {
             approximate_size: Arc::new(AtomicUsize::new(0)),
         }
     }
+
+    /// core functionality 1: get() the pair with a key.
     pub fn get(&self, key: &[u8]) -> Option<Bytes> {
         self.map.get(key).map(|e| e.value().clone())
     }
+
+    /// core functionality 2: put() the entry with a key-value pair.
     pub fn put(&self, key: &[u8], value: &[u8]) -> Result<()> {
+        // pre-calculate the size
         let estimated_size = key.len() + value.len();
+        // insert the key-value pair.
         self.map
             .insert(Bytes::copy_from_slice(key), Bytes::copy_from_slice(value));
+        // update the estimated_size.
         self.approximate_size
             .fetch_add(estimated_size, std::sync::atomic::Ordering::Relaxed);
         Ok(())
     }
 }
 
+/// Define a SkipMap Range-Iterator for `Range Query`, like scan() function.
 type SkipMapRangeIter<'a> =
     crossbeam_skiplist::map::Range<'a, Bytes, (Bound<Bytes>, Bound<Bytes>), Bytes, Bytes>;
 
 // define a self-referential struct, to hold refs to its own fields.
 #[self_referencing]
 pub struct MemTableIterator {
+    // store the map, which contain all the key-value pairs.
     map: Arc<SkipMap<Bytes, Bytes>>,
+
     #[borrows(map)] //the iterator `iter` borrows the `map` field
     #[not_covariant] //the iterator is not Covariant along with the struct.
+    // iter is the actual Iterator when Range-Query is executed.
     iter: SkipMapRangeIter<'this>,
-    item: (Bytes, Bytes), // stores the current key-value pair pointed to by the Iter.
+    // item stores the current key-value pair pointed to by the iter.
+    item: (Bytes, Bytes),
 }
 
 impl MemTableIterator {
+    /// Convert an entry to an item.
     fn entry_to_item(entry: Option<Entry<'_, Bytes, Bytes>>) -> (Bytes, Bytes) {
         entry
             .map(|x| (x.key().clone(), x.value().clone()))
@@ -61,17 +81,27 @@ impl MemTableIterator {
     }
 }
 
+// We need to impl the `StorageIterator`  for MemTableIterator for general purpose.
 impl StorageIterator for MemTableIterator {
+    // appoint the KeyType as KeySlice.
     type KeyType<'a> = KeySlice<'a>;
+
+    // get the current entry's value.
     fn value(&self) -> &[u8] {
+        // borrow_item() provides an `immutable_reference` to item, just like &item.
         &self.borrow_item().1[..]
     }
+
+    // get the current entry's key.
     fn key(&self) -> KeySlice {
         KeySlice::from_slice(&self.borrow_item().0[..])
     }
+
+    // check the validitity of the current entry.
     fn is_valid(&self) -> bool {
         !self.borrow_item().0.is_empty()
     }
+
     /// moves the iterator to the next position.
     fn next(&mut self) -> anyhow::Result<()> {
         let entry = self.with_iter_mut(|iter| MemTableIterator::entry_to_item(iter.next()));
