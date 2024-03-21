e6b2fc7b85c83957c0f69f2295d763ef0b182bc1
zkthernum <kevin91cooland@gmail.com>
2024-03-21 13:56:56 +0800

    finish Simple level compaction skeleton

--- src/compact/simple_leveled.rs
+++ src/compact/simple_leveled.rs
@@ -1,3 +1,17 @@
+#![allow(unused)]
+use serde::{Deserialize, Serialize};
+
+use crate::lsm_storage::LsmStroageState;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SimpleLeveledCompactionTask {
+    upper_level: Option<usize>,
+    upper_level_sst_ids: Vec<usize>,
+    lower_level: usize,
+    lower_level_sst_ids: Vec<usize>,
+    is_lower_level_bottom_level: bool,
+}
+
 pub struct SimpleLeveledCompactionController {
     options: SimpleLeveledCompactionOptions,
 }
@@ -7,4 +21,26 @@ pub struct SimpleLeveledCompactionOptions {
     pub size_ratio_precent: usize,
     pub level0_file_num_compaction_trigger: usize,
     pub max_levels: usize,
+}
+
+impl SimpleLeveledCompactionController {
+    pub fn new(options: SimpleLeveledCompactionOptions) -> Self {
+        todo!()
+    }
+
+    pub fn generate_compaction_task(
+        &self,
+        snapshot: &LsmStroageState,
+    ) -> Option<SimpleLeveledCompactionTask> {
+        todo!()
+    }
+
+    pub fn apply_compaction_result(
+        &self,
+        snapshot: &LsmStroageState,
+        task: &SimpleLeveledCompactionTask,
+        output: &[usize],
+    ) -> (LsmStroageState, Vec<usize>) {
+        todo!()
+    }
 }
