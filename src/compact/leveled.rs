928f53a4e234d139d07ee94b39c492fecaaac720
zkthernum <kevin91cooland@gmail.com>
2024-03-21 22:21:55 +0800

    start Level Compaction submod

--- src/compact/leveled.rs
+++ src/compact/leveled.rs
@@ -1,14 +1,18 @@
 #![allow(unused)]
 
+use std::process::Output;
+
 use serde::{Deserialize, Serialize};
+
+use crate::lsm_storage::LsmStroageState;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct LeveledCompactionTask {
     // if upper_level is None, means L0-compaction.
     pub upper_level: Option<usize>,
-    pub upper_level_ssts_id: usize,
+    pub upper_level_ssts_id: Vec<usize>,
     pub lower_level: usize,
-    pub lower_level_ssts_id: usize,
+    pub lower_level_ssts_id: Vec<usize>,
     pub is_lower_level_the_bottom: bool,
 }
 
@@ -22,4 +26,35 @@ pub struct LeveledCompactionOptions {
     pub level0_files_num_compaction_threshold: usize,
     pub max_levels: usize,
     pub base_level_size_mb: usize,
+}
+
+impl LeveledCompactionController {
+    pub fn new(options: LeveledCompactionOptions) -> Self {
+        Self { options }
+    }
+
+    fn find_overlaping_ssts(
+        &self,
+        snapshot: &LsmStroageState,
+        sst_ids: &[usize],
+        in_level: usize,
+    ) -> Vec<usize> {
+        todo!()
+    }
+
+    pub fn generate_compaction_task(
+        &self,
+        snapshot: &LsmStroageState,
+    ) -> Option<LeveledCompactionTask> {
+        todo!()
+    }
+
+    pub fn apply_compaction_result(
+        &self,
+        snapshot: &LsmStroageState,
+        task: &LeveledCompactionTask,
+        output: &[usize],
+    ) -> (LsmStroageState, Vec<usize>) {
+        todo!()
+    }
 }
