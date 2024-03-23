37dc2b35d08cccb1cbc10efd8b702d4803b90663
zkthernum <kevin91cooland@gmail.com>
2024-03-23 20:07:41 +0800

    add Tiered Compaction skeleton

--- src/compact/tiered.rs
+++ src/compact/tiered.rs
@@ -2,6 +2,8 @@
 
 use serde::{Deserialize, Serialize};
 
+use crate::lsm_storage::LsmStroageState;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TieredCompactionTask {
     pub tiers: Vec<(usize, Vec<usize>)>,
@@ -15,4 +17,29 @@ pub struct TieredCompactionController {
 #[derive(Debug, Clone)]
 pub struct TieredCompactionOptions {
     pub level_size_multiplier: usize,
+    pub max_size_amplification_percent: usize,
+    pub size_ratio: usize,
+    pub min_merge_width: usize,
+}
+
+impl TieredCompactionController {
+    pub fn new(options: TieredCompactionOptions) -> Self {
+        Self { options }
+    }
+
+    pub fn generate_compaction_task(
+        &self,
+        snapshot: &LsmStroageState,
+    ) -> Option<TieredCompactionTask> {
+        todo!()
+    }
+
+    pub fn apply_compaction_result(
+        &self,
+        snapshot: &LsmStroageState,
+        task: &TieredCompactionTask,
+        output: &[usize],
+    ) -> (LsmStroageState, Vec<usize>) {
+        todo!()
+    }
 }
