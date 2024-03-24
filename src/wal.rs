bb270042d826b410b264f841135f04b7c1473a8e
zkthernum <kevin91cooland@gmail.com>
2024-03-24 16:58:48 +0800

    finish WAL mod

--- src/wal.rs
+++ src/wal.rs
@@ -3,16 +3,16 @@
 use std::{
     fs::{File, OpenOptions},
     hash::Hasher,
-    io::{BufWriter, Write},
+    io::{BufWriter, Read, Write},
     path::Path,
     sync::Arc,
 };
 
-use parking_lot:: {Mutex}
+use parking_lot::Mutex;
 
 use anyhow::{bail, Context, Ok, Result};
 
-use bytes::{BufMut, Bytes};
+use bytes::{Buf, BufMut, Bytes};
 use crossbeam_skiplist::SkipMap;
 
 pub struct Wal {
@@ -34,7 +34,37 @@ impl Wal {
     }
 
     pub fn recover(path: impl AsRef<Path>, skiplist: &SkipMap<Bytes, Bytes>) -> Result<Self> {
-        todo!()
+        let mut file = OpenOptions::new()
+            .read(true)
+            .append(true)
+            .open(path)
+            .context("failed to open the wal")?;
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf);
+        let mut buf_ptr = &buf[..];
+        while buf_ptr.has_remaining() {
+            let mut hasher = crc32fast::Hasher::new();
+            // get the key
+            let key_len = buf_ptr.get_u16() as usize;
+            hasher.write_u16(key_len as u16);
+            let key = Bytes::copy_from_slice(&buf_ptr[..key_len]);
+            hasher.write(&key);
+            buf_ptr.advance(key_len);
+            // get the value
+            let value_len = buf_ptr.get_u16() as usize;
+            hasher.write_u16(value_len as u16);
+            let value = Bytes::copy_from_slice(&buf_ptr[..value_len]);
+            hasher.write(&value);
+            buf_ptr.advance(value_len);
+            // get the checksum and validate
+            if hasher.finalize() != buf_ptr.get_u32() {
+                bail!("checksum mismatched!");
+            }
+            skiplist.insert(key, value);
+        }
+        Ok(Self {
+            file: Arc::new(Mutex::new(BufWriter::new(file))),
+        })
     }
 
     pub fn put(&self, key: &[u8], value: &[u8]) -> Result<()> {
@@ -52,7 +82,7 @@ impl Wal {
         buf.put_u16(value.len() as u16);
         hasher.write(value);
         buf.put_slice(value);
-        // add checksum 
+        // add checksum
         buf.put_u32(hasher.finalize());
         file.write_all(&buf)?;
         Ok(())
