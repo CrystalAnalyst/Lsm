620ff40f7d033782dac8a48ccb2e864b5a20e8ab
zkthernum <kevin91cooland@gmail.com>
2024-03-24 16:38:29 +0800

    add put() for WAL

--- src/wal.rs
+++ src/wal.rs
@@ -12,7 +12,7 @@ use parking_lot:: {Mutex}
 
 use anyhow::{bail, Context, Ok, Result};
 
-use bytes::Bytes;
+use bytes::{BufMut, Bytes};
 use crossbeam_skiplist::SkipMap;
 
 pub struct Wal {
@@ -42,8 +42,20 @@ impl Wal {
         let mut buf: Vec<u8> =
             Vec::with_capacity(key.len() + value.len() + std::mem::size_of::<u16>());
         let mut hasher = crc32fast::Hasher::new();
+        // Write key length and key.
         hasher.write_u16(key.len() as u16);
-        todo!()
+        buf.put_u16(key.len() as u16);
+        hasher.write(key);
+        buf.put_slice(key);
+        // Write value length and value.
+        hasher.write_u16(value.len() as u16);
+        buf.put_u16(value.len() as u16);
+        hasher.write(value);
+        buf.put_slice(value);
+        // add checksum 
+        buf.put_u32(hasher.finalize());
+        file.write_all(&buf)?;
+        Ok(())
     }
 
     pub fn sync(&self) -> Result<()> {
