53d7ec8f07fd9da0eb18063a239ef6e12cfe448d
zkthernum <kevin91cooland@gmail.com>
2024-03-25 12:59:10 +0800

    start Tiered compaction: Space Amplification ratio trigger

--- src/compact/tiered.rs
+++ src/compact/tiered.rs
@@ -4,6 +4,8 @@ use serde::{Deserialize, Serialize};
 
 use crate::lsm_storage::LsmStroageState;
 
+/// represents a compaction task, which includes the tiers
+/// to comapct and whether the bottom tier is included.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TieredCompactionTask {
     pub tiers: Vec<(usize, Vec<usize>)>,
@@ -16,7 +18,7 @@ pub struct TieredCompactionController {
 
 #[derive(Debug, Clone)]
 pub struct TieredCompactionOptions {
-    pub level_size_multiplier: usize,
+    pub num_of_tiers: usize,
     pub max_size_amplification_percent: usize,
     pub size_ratio: usize,
     pub min_merge_width: usize,
@@ -31,6 +33,30 @@ impl TieredCompactionController {
         &self,
         snapshot: &LsmStroageState,
     ) -> Option<TieredCompactionTask> {
+        // 0. Precondition check
+        assert!(
+            snapshot.l0_sstables.is_empty(),
+            "L0 should not present in Tiered compaction"
+        );
+        if snapshot.levels.len() < self.options.num_of_tiers {
+            return None;
+        }
+        // 1.compaction triggered by space Amplification ratio
+        let mut size = 0;
+        for id in 0..(snapshot.levels.len() - 1) {
+            size += snapshot.levels[id].1.len();
+        }
+        let space_amp_ratio =
+            (size as f64) / (snapshot.levels.last().unwrap().1.len() as f64) * 100.0;
+        if space_amp_ratio >= self.options.max_size_amplification_percent as f64 {
+            return Some(TieredCompactionTask {
+                tiers: snapshot.levels.clone(),
+                bottom_tier_included: true,
+            });
+        }
+        // 2. size ratio check
+        let size_ratio_trigger = (100.0 + self.options.size_ratio as f64) / 100.0;
+        let mut size = 0;
         todo!()
     }
 
