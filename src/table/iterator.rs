56b5cb7fb24e3d89261c9b3f5ca25dc81538faba
zkthernum <kevin91cooland@gmail.com>
2024-03-26 14:08:19 +0800

    impl StroageIterator Trait for SSTableIterator

--- src/table/iterator.rs
+++ src/table/iterator.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
-use crate::block::iterator::BlockIterator;
+use crate::{block::iterator::BlockIterator, iterators::StorageIterator, key::KeySlice};
+use anyhow::{Ok, Result};
 
 use super::SsTable;
 
@@ -9,4 +10,33 @@ pub struct SsTableIterator {
     table: Arc<SsTable>,
     block_iter: BlockIterator,
     block_idx: usize,
+}
+
+impl StorageIterator for SsTableIterator {
+    type KeyType<'a> = KeySlice<'a>;
+
+    fn value(&self) -> &[u8] {
+        self.block_iter.value()
+    }
+
+    fn key(&self) -> KeySlice {
+        self.block_iter.key()
+    }
+
+    fn is_valid(&self) -> bool {
+        self.block_iter.is_valid()
+    }
+
+    fn next(&mut self) -> Result<()> {
+        self.block_iter.next();
+        if !self.block_iter.is_valid() {
+            self.block_idx += 1;
+            if self.block_idx < self.table.num_of_blocks() {
+                self.block_iter = BlockIterator::create_and_seek_to_first(
+                    self.table.read_block_cached(self.block_idx)?,
+                );
+            }
+        }
+        Ok(())
+    }
 }
