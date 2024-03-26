350e3da945137d44c196a37b37c4899500cc128e
zkthernum <kevin91cooland@gmail.com>
2024-03-26 19:22:58 +0800

    finish TwoMergeIterator submod

--- src/iterators/two_merge_iterator.rs
+++ src/iterators/two_merge_iterator.rs
@@ -1,10 +1,48 @@
 use super::StorageIterator;
+use anyhow::{Ok, Result};
 
 pub struct TwoMergeIterator<A: StorageIterator, B: StorageIterator> {
     a: A,
     b: B,
     // a flag to indicate that whether choose a or b.
     choose_a: bool,
+}
+
+impl<
+        A: 'static + StorageIterator,
+        B: 'static + for<'a> StorageIterator<KeyType<'a> = A::KeyType<'a>>,
+    > TwoMergeIterator<A, B>
+{
+    // constructor
+    pub fn create(a: A, b: B) -> Result<Self> {
+        let mut iter = Self {
+            a,
+            b,
+            choose_a: false,
+        };
+        iter.skip_b()?;
+        iter.choose_a = Self::choose_a(&a, &b);
+        Ok(iter)
+    }
+
+    fn choose_a(a: &A, b: &B) -> bool {
+        if !a.is_valid() {
+            return false;
+        }
+        if !b.is_valid() {
+            return false;
+        }
+        // If a.key() is smaller then return true
+        // otherwise return false, means that a.key() is the larger one.
+        a.key() < b.key()
+    }
+
+    fn skip_b(&mut self) -> Result<()> {
+        if self.a.is_valid() && self.b.is_valid() && self.b.key() == self.a.key() {
+            self.b.next()?;
+        }
+        Ok(())
+    }
 }
 
 impl<
