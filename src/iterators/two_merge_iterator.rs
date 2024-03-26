6bd625d41de19b700593f5d50be76fedb10e9f43
zkthernum <kevin91cooland@gmail.com>
2024-03-26 19:15:49 +0800

    Impl StorageIterator for TwoMergeIterator

--- src/iterators/two_merge_iterator.rs
+++ src/iterators/two_merge_iterator.rs
@@ -3,5 +3,53 @@ use super::StorageIterator;
 pub struct TwoMergeIterator<A: StorageIterator, B: StorageIterator> {
     a: A,
     b: B,
+    // a flag to indicate that whether choose a or b.
     choose_a: bool,
+}
+
+impl<
+        A: 'static + StorageIterator,
+        B: 'static + for<'a> StorageIterator<KeyType<'a> = A::KeyType<'a>>,
+    > StorageIterator for TwoMergeIterator<A, B>
+{
+    type KeyType<'a> = A::KeyType<'a>;
+
+    fn is_valid(&self) -> bool {
+        if self.choose_a {
+            self.a.is_valid()
+        } else {
+            self.b.is_valid()
+        }
+    }
+
+    fn key(&self) -> Self::KeyType<'_> {
+        if self.choose_a {
+            self.a.key()
+        } else {
+            self.b.key()
+        }
+    }
+
+    fn value(&self) -> &[u8] {
+        if self.choose_a {
+            self.a.value()
+        } else {
+            self.b.value()
+        }
+    }
+
+    fn next(&mut self) -> anyhow::Result<()> {
+        if self.choose_a {
+            self.a.next()?;
+        } else {
+            self.b.next()?;
+        }
+        self.skip_b()?;
+        self.choose_a = Self::choose_a(&self.a, &self.b);
+        Ok(())
+    }
+
+    fn number_of_iterators(&self) -> usize {
+        self.a.number_of_iterators() + self.b.number_of_iterators()
+    }
 }
